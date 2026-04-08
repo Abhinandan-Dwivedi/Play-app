a24187bda00dc3928e8395297277e81c199c4822
Abhinandan <dwabhi2006@gmail.com>
2026-04-09 01:00:55 +0530

    url change

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,13 +6,15 @@ import { User } from './Models/User.model.js';
 
 const app = express();
 
-// app.use (cors({
-//     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-//     credentials: true
-// }));
+app.use(cors({
+    origin: 'https://play-app-frontend-seven.vercel.app',  
+    credentials: true, 
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
-const frontendOrigin = "https://play-video-ten.vercel.app";
-app.use(cors({ origin: frontendOrigin, credentials: true }));
+// const frontendOrigin = 'https://play-app-frontend-seven.vercel.app/';
+// app.use(cors({ origin: frontendOrigin, credentials: true }));
 
 app.use(express.json({ limit: "50kb" }));
 app.use(express.urlencoded({ extended: true, limit: "50kb" }));
